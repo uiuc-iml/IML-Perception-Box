3b1a176382f3a1de907446731592b6b50ab9a7ea
Aadityavoru <av34@illinois.edu>
2025-02-28 14:52:12 -0600

    added IMU to camera

---
+++ build/CMakeFiles/imu.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/motion/Perception-Box/src/imu.cc" "CMakeFiles/imu.dir/src/imu.cc.o" "gcc" "CMakeFiles/imu.dir/src/imu.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/imu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imu.dir/src/imu.cc.o"
+  "CMakeFiles/imu.dir/src/imu.cc.o.d"
+  "imu"
+  "imu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
